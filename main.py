16b5a3ccce1febca7d2c010780efa71dbc992403
Hadi Tedi <haditedi@gmail.com>
2023-08-09 11:54:33 +0100

    all working !

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ def index():
    print(response) 
    return jsonify({"result" : response})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug = True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug = True)
