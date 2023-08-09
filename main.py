c3cf5473b8c9f7d4548ca10c1105d395384db899
Hadi Tedi <haditedi@gmail.com>
2023-08-09 18:43:39 +0100

    added limiter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from dotenv import load_dotenv
 import os
 import openai, requests
@@ -8,11 +10,23 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]
+)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+env_variable = os.getenv("DEV")
 
 @app.route("/", methods =["POST","GET"])
+@limiter.limit("1 per day")
 def index():
    data = request.json
+   
+   if data["key"] != "123456":
+       print("ABORT")
+       return make_response(jsonify({"error":"something went wrong"}), 500)
+    
    data = data["animal"]
    for item in data:
        if "id" in item:
@@ -26,5 +40,6 @@ def index():
    print(response) 
    return jsonify({"result" : response})
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug = True)
+if env_variable == "development":
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", debug = True)
