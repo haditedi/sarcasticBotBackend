4d8defa987d9b2f1a63ffe8b449075c5bce8c4d5
Hadi Tedi <haditedi@gmail.com>
2023-08-10 21:22:03 +0100

    removed a forward slash

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 # CORS(app)
-CORS(app, resources={r"/api/*": {"origins": "https://tedi-ai.vercel.app/"}})
+CORS(app, resources={r"/api/*": {"origins": "https://tedi-ai.vercel.app"}})
 # limiter = Limiter(
 #     get_remote_address,
 #     app=app,
